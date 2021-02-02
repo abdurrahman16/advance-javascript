d13b5f336f3eda6ec298afed3051a5d841e9bfe6
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-02-02 15:30:21 +0600

    slice splice join

---
+++ array.js
@@ -0,0 +1,9 @@
+const numbers =[1,2,3,4,5,6,7];
+
+// const part = numbers.slice(3,5);
+const removed = numbers.splice(2,3);
+console.log(removed);
+
+const together = numbers.join(" ami ");
+console.log(together);
+
