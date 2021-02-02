751badae299d4d221d8ba4605945d1efd1acfc11
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-02-02 15:20:10 +0600

    closure function

--- index.js
+++ index.js
@@ -38,11 +38,30 @@
 // console.log(filters);
 
 
-function sum (firs,second){
-    let result = first+second;
-    return result;
+// function sum (firs,second){
+//     let result = first+second;
+//     return result;
+// }
+
+// const output = sum(3,7);
+// console.log(result);
+// console.log(output);
+
+
+// closure.js
+
+function stopWatch (){
+    let count =0;
+    return function(){
+    count ++;
+    return count;
+    }
 }
 
-const output = sum(3,7);
-console.log(result);
-console.log(output);
+const clock1 = stopWatch();
+// clock1();
+// clock1();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
